731a0ee49ad7368e511f367b2f7b54c48bd1576a
Ilya <olovyannikov_barista@icloud.com>
2021-07-21 20:47:33 +0300

    Адаптив

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,14 @@
 import 'bootstrap/dist/js/bootstrap.bundle';
-import { Fancybox } from "@fancyapps/ui";
+import {Fancybox} from "@fancyapps/ui";
 import './vendor/gridfy.vendor';
 
 window.addEventListener('DOMContentLoaded', () => {
 
-    Fancybox.bind("[data-fancybox]", {
-
-    });
+    Fancybox.bind("[data-fancybox]", {});
 
     const container = document.querySelector('#gallery');
     const path = './img/';
     let images = [];
-
 
 
     const imagesFn = () => {
@@ -33,11 +30,18 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 
     const getBigPhotos = () => {
+        if (window.matchMedia('(max-width: 767px)').matches) {
+            document.querySelectorAll('img').forEach(image => {
+                image.style.width = '100%'
+                image.style.position = 'static'
+            });
+        }
+
         if (window.matchMedia('(min-width: 768px)').matches) {
             container.gridify({
                 srcNode: 'img',             // grid items (class, node)
                 margin: '20px',             // margin in pixel, default: 0px
-                width: '300px',             // grid item width in pixel, default: 220px
+                width: '320px',             // grid item width in pixel, default: 220px
                 max_width: '',              // dynamic gird item width if specified, (pixel)
                 resizable: true,            // re-layout if window resize
                 transition: 'all 0.5s ease' // support transition for CSS3, default: all 0.5s ease
